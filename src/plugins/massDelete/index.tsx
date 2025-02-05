3a723e58dfc149b5508b2ea9b4dfce53984aa844
sinjs <50749166+sinjs@users.noreply.github.com>
2025-02-05 18:47:50 +0100

    fix: clean up mass delete

--- src/plugins/massDelete/index.tsx
+++ src/plugins/massDelete/index.tsx
@@ -4,15 +4,36 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { ApplicationCommandInputType, ApplicationCommandOptionType } from "@api/Commands";
+import { ApplicationCommandInputType, ApplicationCommandOptionType, findOption, sendBotMessage } from "@api/Commands";
 import { Devs } from "@utils/constants";
-import { ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalRoot, openModal } from "@utils/modal";
 import definePlugin from "@utils/types";
-import { Button, Constants, Forms, MessageStore, RestAPI, showToast, Text, Toasts, UserStore } from "@webpack/common";
-import { Message } from "discord-types/general";
+import { MessageActions, MessageStore, UserStore } from "@webpack/common";
+import { Channel, Message } from "discord-types/general";
 
-function sleep(ms: number) {
-    return new Promise<void>(resolve => setTimeout(() => resolve(), ms));
+function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(() => resolve(), ms));
+}
+
+/**
+ * Deletes an amount of messages in a specified channel with a delay
+ * @returns The amount of messages actually deleted
+ */
+async function deleteMessages(amount: number, channel: Channel, delay: number = 1500): Promise<number> {
+    let deleted = 0;
+    const userId = UserStore.getCurrentUser().id;
+
+    // Have to reserialize to remove object methods and make it at least semi-compliant with the `Message` type
+    const messages: Message[] = JSON.parse(JSON.stringify(MessageStore.getMessages(channel.id)._array.filter((m: Message) => m.author.id === userId).reverse()));
+
+    for (const message of messages) {
+        MessageActions.deleteMessage(channel.id, message.id);
+        amount--;
+        deleted++;
+        if (amount === 0) break;
+        await sleep(delay);
+    }
+
+    return deleted;
 }
 
 export default definePlugin({
@@ -23,119 +44,37 @@ export default definePlugin({
     commands: [{
         name: "purge",
         description: "Purges an amount of messages from a channel",
-        options: [{
-            name: "amount",
-            description: "Amount of messages to delete",
-            type: ApplicationCommandOptionType.NUMBER,
-            required: true,
-        }],
-        inputType: ApplicationCommandInputType.BUILT_IN,
-
-
-        execute(args, ctx) {
-            const amount = parseInt(args[0].value);
-            const userId = UserStore.getCurrentUser().id;
-            const channelId = ctx.channel.id;
-
-
-
-            async function onConfirm() {
-                let before = MessageStore.getMessages(channelId).reduce((prev, current) => (prev && prev.timestamp.getTime() > current.timestamp.getTime()) ? prev : current);
-
-                const loadMessagesRest = async () => await RestAPI.get({ url: Constants.Endpoints.MESSAGES(channelId), query: { before, limit: 100 } });
-
-                const loadMessages = async (): Promise<Message[] | false> => {
-                    const response = await loadMessagesRest()
-                        .then(response => response.body as Message[])
-                        .catch(async error => {
-                            if (error.status === 429 && error.body.retry_after) {
-                                showToast(`You have been rate limited. Waiting ${error.body.retry_after}s`, Toasts.Type.FAILURE);
-                                await sleep(error.body.retry_after * 1000 + 100);
-                                return await loadMessagesRest().then(response => response.body as Message[]);
-                            }
-
-                            showToast(`Error while loading messages: ${error}`, Toasts.Type.FAILURE);
-                            throw error;
-                        });
-                    if (!response.length) return false;
-                    return response.filter(m => m.author.id === userId);
-                };
-
-                const deleteMessageRest = async (messageId: string) => await RestAPI.del({
-                    url: Constants.Endpoints.MESSAGE(channelId, messageId)
-                });
-
-                const deleteMessage = async (messageId: string) => {
-                    await deleteMessageRest(messageId).catch(async error => {
-                        if (error.status === 429 && error.body.retry_after) {
-                            showToast(`You have been rate limited. Waiting ${error.body.retry_after}s`, Toasts.Type.FAILURE);
-                            await sleep(error.body.retry_after * 1000 + 100);
-                            return await deleteMessage(messageId);
-                        }
-
-                        showToast(`Error while deleting messages: ${error}`, Toasts.Type.FAILURE);
-                        throw error;
-                    });
-                };
-
-                let messages = await loadMessages();
-
-                if (!messages) return showToast("No more messages to delete!", Toasts.Type.FAILURE);
-
-                console.log(messages);
-
-                for (let n = 0; n < amount; n++) {
-                    let message = messages[n];
-
-                    if (!message) {
-                        while (!message) {
-                            await sleep(200);
-                            before = messages[n - 1].id;
-                            const newMessages = await loadMessages();
-                            if (!newMessages) return showToast("No more messages to delete!", Toasts.Type.FAILURE);
-                            messages = messages.concat(...newMessages);
-                            message = messages[n];
-                        }
-                    }
-
-                    await sleep(200);
-                    await deleteMessage(message.id);
-
-                    console.log("Deleted message", message.content, `(${message.id})`);
-                }
-
-                showToast(`Successfully deleted ${amount} messages!`, Toasts.Type.SUCCESS);
+        options: [
+            {
+                name: "amount",
+                description: "How many messages you want to purge",
+                type: ApplicationCommandOptionType.INTEGER,
+                required: true
+            },
+            {
+                name: "channel",
+                description: "Channel ID you want to purge from",
+                type: ApplicationCommandOptionType.CHANNEL,
+                required: false
+            },
+            {
+                name: "delay",
+                description: "Delay inbetween deleting messages",
+                type: ApplicationCommandOptionType.INTEGER,
+                required: false
             }
+        ],
+        inputType: ApplicationCommandInputType.BUILT_IN,
+        async execute(args, ctx) {
+            const amount: number = findOption(args, "amount", 0);
+            const channel: Channel = findOption(args, "channel", ctx.channel);
+            const delay: number = findOption(args, "delay", 1500);
 
-            openModal(props => <ModalRoot {...props}>
-                <ModalHeader className="vc-st-modal-header">
-                    <Forms.FormTitle tag="h2">
-                        Mass Delete
-                    </Forms.FormTitle>
+            sendBotMessage(ctx.channel.id, { content: `> Deleting ${amount} messages in <#${channel.id}>` });
 
-                    <ModalCloseButton onClick={close} />
-                </ModalHeader>
-                <ModalContent>
-                    <Text variant="text-md/normal">Are you sure you want to mass delete {args[0].value} messages?</Text>
-                    <Text variant="text-md/normal">WARNING: Too many deletions might cause your account to get flagged.</Text>
-                </ModalContent>
+            const deleted = await deleteMessages(amount, channel, delay);
 
-                <ModalFooter>
-                    <Button
-                        color={Button.Colors.TRANSPARENT}
-                        look={Button.Looks.LINK}
-                        onClick={() => props.onClose()}
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        color={Button.Colors.RED}
-                        onClick={() => { props.onClose(); onConfirm(); }}
-                    >
-                        Delete
-                    </Button>
-                </ModalFooter>
-            </ModalRoot >);
+            sendBotMessage(ctx.channel.id, { content: `> Deleted ${deleted} messages` });
         },
     }]
 });
