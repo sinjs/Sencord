7c819247bc52bb421288abf00461c68ca14da09d
sinjs <50749166+sinjs@users.noreply.github.com>
2025-02-06 16:44:49 +0100

    feat: included themes

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -10,13 +10,56 @@
 
 import { UserThemeHeader } from "@main/themes";
 
-import redTheme from "./red.css?managed";
+import hideAppButton from "./hideAppButton.css?managed";
+import hideConfettiButton from "./hideConfettiButton.css?managed";
+import hideGifButton from "./hideGifButton.css?managed";
+import hideGiftButton from "./hideGiftButton.css?managed";
+import hidePlayAgain from "./hidePlayAgain.css?managed";
+import hideShop from "./hideShop.css?managed";
+import hideStickerButton from "./hideStickerButton.css?managed";
+
 
 export const themes: UserThemeHeader[] = [
     {
-        name: "red",
-        author: "1",
-        description: "Makes everything red",
-        fileName: redTheme
+        name: "HidePlayAgain",
+        author: "sin",
+        description: "Hide the Play Again buttons above the direct messages",
+        fileName: hidePlayAgain
+    },
+    {
+        name: "HideGiftButton",
+        author: "sin",
+        description: "Hide the gift button in the chat bar",
+        fileName: hideGiftButton
+    },
+    {
+        name: "HideConfettiButton",
+        author: "sin",
+        description: "Hide the confetti button in the chat bar",
+        fileName: hideConfettiButton
+    },
+    {
+        name: "HideShop",
+        author: "sin",
+        description: "Hide the Shop and Nitro Store",
+        fileName: hideShop
+    },
+    {
+        name: "HideStickerButton",
+        author: "sin",
+        description: "Hide the sticker picker button in the chat bar",
+        fileName: hideStickerButton
+    },
+    {
+        name: "HideGifButton",
+        author: "sin",
+        description: "Hide the GIF picker button in the chat bar",
+        fileName: hideGifButton
+    },
+    {
+        name: "HideAppButton",
+        author: "sin",
+        description: "Hide the app launcher button in the chat bar",
+        fileName: hideAppButton
     }
 ];
