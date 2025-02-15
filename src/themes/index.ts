090ee47580011c2fc0c8398f75b529a320135b11
sinjs <50749166+sinjs@users.noreply.github.com>
2025-02-15 17:45:07 +0100

    feat: more themes

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -10,6 +10,8 @@
 
 import { UserThemeHeader } from "@main/themes";
 
+import amoledCord from "./amoledCord.css?managed";
+import compactPlusPlus from "./compactPlusPlus.css?managed";
 import hideAppButton from "./hideAppButton.css?managed";
 import hideConfettiButton from "./hideConfettiButton.css?managed";
 import hideGifButton from "./hideGifButton.css?managed";
@@ -17,9 +19,37 @@ import hideGiftButton from "./hideGiftButton.css?managed";
 import hidePlayAgain from "./hidePlayAgain.css?managed";
 import hideShop from "./hideShop.css?managed";
 import hideStickerButton from "./hideStickerButton.css?managed";
+import system24 from "./system24.css?managed";
 
 
 export const themes: UserThemeHeader[] = [
+    {
+        name: "AmoledCord",
+        author: "LuckFire",
+        description: "A basically pitch black theme for Discord. Lights out, baby!",
+        version: "4.1.10",
+        source: "https://github.com/LuckFire/amoled-cord",
+        invite: "vYdXbEzqDs",
+        fileName: amoledCord
+    },
+    {
+        name: "Compact++",
+        author: "Grzesiek11",
+        description: "Makes Discord's Compact mode more like IRC (and thus better)",
+        version: "1.5.6",
+        source: "https://gitlab.com/Grzesiek11/compactplusplus-discord-theme",
+        fileName: compactPlusPlus,
+    },
+    {
+        name: "System24",
+        author: "refact0r",
+        description: "A tui-style discord theme.",
+        version: "1.0.0",
+        source: "https://github.com/refact0r/system24/blob/master/system24.theme.css",
+        website: "https://www.refact0r.dev",
+        invite: "nz87hXyvcy",
+        fileName: system24,
+    },
     {
         name: "HidePlayAgain",
         author: "sin",
