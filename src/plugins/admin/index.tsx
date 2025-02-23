bbb6a4e562c939bb9d9621699f5569b1a7cc1869
sin! <50749166+sinjs@users.noreply.github.com>
2025-02-23 21:39:13 +0000

    fix: remove client-sided admin check

--- src/plugins/admin/index.tsx
+++ src/plugins/admin/index.tsx
@@ -420,11 +420,6 @@ export default definePlugin({
 
     toolboxActions: {
         async "Open Administration"() {
-            if (!((UserStore.getCurrentUser().id == "582735975151828992") || (UserStore.getCurrentUser().id == "778328725208301588"))) {
-                Toasts.show({ id: "admin-unauthorized", message: "You are not allowed to view this resource", type: Toasts.Type.FAILURE });
-                return;
-            }
-
             const callback = () => openModal(modalProps => <AdminModal modalProps={modalProps} />);
             const auth = await getAuth();
 
